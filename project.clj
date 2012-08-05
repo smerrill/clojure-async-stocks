83cd3a9e1643cd0a1a5595e1522a179737153baf
Steven Merrill <steven.merrill@gmail.com>
2012-08-05 13:33:50 -0400

    Fix agents in the REPL/nailgun. Derp.

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject async-stocks "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [clj-http "0.5.2"]])
+                 [clj-http "0.5.2"]]
+  :shutdown-agents true)
