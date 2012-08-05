4bfee99ec0449a17f2ca05c4425124184c5e1304
Steven Merrill <steven.merrill@gmail.com>
2012-08-05 12:44:32 -0400

    Add clj-http.

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject async-stocks "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [clj-http "0.5.2"]])
