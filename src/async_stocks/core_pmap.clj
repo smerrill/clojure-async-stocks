b7ec1689e7c7c0acbd14da9c6aeedecdffa5a05c
Steven Merrill <steven.merrill@gmail.com>
2012-08-07 16:55:04 -0400

    Kill the unused agent code.

--- src/async_stocks/core_pmap.clj
+++ src/async_stocks/core_pmap.clj
@@ -4,7 +4,6 @@
 
 (def symbols ["AAPL" "GOOG" "IBM" "MSFT"])
 (def year 2008)
-(def highest-price (agent {}))
 
 (defn stock-price-url [symbol year]
   "Return the URL to grab CSV data about a stock's closing price."
@@ -15,14 +14,4 @@
   (let [{body :body} (client/get (stock-price-url symbol year))]
     {:symbol symbol :close (Float/parseFloat (get (string/split (last (string/split-lines body)) #",") 2))}))
 
-;(defn compare-price [a b]
-  ;(compare (get a :close) (get b :close)))
-
-; Send off the actions to the actor, then wait up to 10 seconds for them to return.
 (apply max-key :close (pmap #(get-stock-data % year) symbols))
-;(-> (send-off highest-price )
-;(map #(send-off highest-price compare-price % year) symbols)
-;(await-for 10000 highest-price)
-
-;(let [{:keys [close symbol]} @highest-price]
-  ;(println (format "The highest stock in %d was %s with closing price %f." year symbol close)))
